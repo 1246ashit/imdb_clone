b2a4db22bfb2e0990ee398bb3752da029bae6a74
jj <1246ashit@gmail.com>
2024-05-12 15:45:44 +0800

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,28 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "./components/Header";
+import Provider from "@/Provider";
+import Navbar from "./components/Navbar";
+import SearchBox from "./components/SearchBox";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "IMDB clone",
+  description: "IMDB clone",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Provider>
+          <Header />
+          <Navbar/>
+          <SearchBox/>
+          {children}
+        </Provider>
+      </body>
+    </html>
+  );
+}
